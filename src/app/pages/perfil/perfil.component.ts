01cfa14332ae19213b4b067e7c755eac12bd3d0c
unknown <gmraivan@gmail.com>
2022-11-28 12:21:29 -0500

    Se agrego la funcionalidad de subir imagenes

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage,ref ,uploadBytes, list, listAll,getDownloadURL} from '@angular/fire/storage';
+
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -9,15 +11,21 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 export class PerfilComponent implements OnInit {
   public usuario: any;
   public imgTemp: any;
-  constructor(private usuarioService: UsuarioService) { }
+  public archivoSubir:any;
+  constructor(private usuarioService: UsuarioService,
+              private storage:Storage) { }
 
   ngOnInit(): void {
     this.usuario = this.usuarioService.usuario
+    this.recuperarImagen()
   }
   cambiarImagen(evento: any) {
     const archivo = evento.target.files[0]
+    this.archivoSubir=evento.target.files[0]
     if (!archivo) {
+      this.archivoSubir=null
       return this.imgTemp = null
+
     }
     const reader = new FileReader()
     const url64 = reader.readAsDataURL(archivo)
@@ -29,6 +37,25 @@ export class PerfilComponent implements OnInit {
 
   }
   cancelarimg(){
+    this.archivoSubir=null
     return this.imgTemp=null
+  }
+  subirimg(){
+    console.log(this.archivoSubir)
+    const imgRef= ref(this.storage,`images/${this.archivoSubir.name}`)
+    uploadBytes(imgRef,this.archivoSubir).then(
+      resp=>{
+        console.log(resp)
+        getDownloadURL(imgRef).then(
+          resp=>console.log(resp)
+        )
+
+      }
+
+    ).catch(
+      error=>console.log(error)
+    )
+  }
+  recuperarImagen(){
   }
 }
